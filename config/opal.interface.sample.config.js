d2975774e19c014f223ca705b8bff8dcb430a702
Axel Oehmichen <axelfrancois.oehmichen11@imperial.ac.uk>
2018-04-30 16:05:30 +0100

    first bits for the different docker compose configs, bash scripts etc

---
+++ config/opal.interface.sample.config.js
@@ -0,0 +1,9 @@
+module.exports = {
+    mongoURL: 'mongodb://[username:password@]host1[:port1][,host2[:port2],...[,hostN[:portN]]][/[database][?options]]',
+    port: 8080,
+    enableCors: true,
+    cacheURL: 'http://cache:8080',
+    algoServiceURL: 'http://algoService:80',
+    algorithmsDirectory: 'algorithms',
+    auditDirectory: 'audit'
+};
---
+++ start.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env bash
+
+echo -e 'Hang tight, the OPAL platform is about to start.'
+
+cd Docker
+
+docker-compose up
