74c3746790bc270eb32e6f1e646bbc321eb4dce8
Axel Oehmichen <axelfrancois.oehmichen11@imperial.ac.uk>
2018-05-24 14:37:16 +0100

    adding missing parameter for scheduler and set the archiving of the jobs to false

--- config/eae.scheduler.sample.config.js
+++ config/eae.scheduler.sample.config.js
@@ -3,6 +3,7 @@ module.exports = {
     mongoURL: 'mongodb://mongodb/opal',
     port: 80,
     enableCors: true,
+    archivingEnabled: false,
     jobsExpiredStatusTime: 720 , // Time in hours: 24h * 30d. Jobs to be archived
     jobsTimingoutTime: 24 , // Time in hours: 24h. Jobs to be cancelled for exceeding computing time policy.
     nodesExpiredStatusTime: 1, // Time in hours. Tolerance for the refresh of the nodes' status
