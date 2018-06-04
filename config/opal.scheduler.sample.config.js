7dc024bb2b893d81e413b512cd8efe904e64ec1c
Axel Oehmichen <axelfrancois.oehmichen11@imperial.ac.uk>
2018-06-04 16:39:03 +0100

    update of the refresh rate for the scheduler

--- config/opal.scheduler.sample.config.js
+++ config/opal.scheduler.sample.config.js
@@ -6,7 +6,7 @@ module.exports = {
     archivingEnabled: true,
     jobsExpiredStatusTime: 720 , // Time in hours: 24h * 30d. Jobs to be archived
     jobsTimingoutTime: 24 , // Time in hours: 24h. Jobs to be cancelled for exceeding computing time policy.
-    nodesExpiredStatusTime: 1, // Time in hours. Tolerance for the refresh of the nodes' status
+    nodesExpiredStatusTime: 10, // Time in seconds. Tolerance for the refresh of the nodes' status
     swiftURL: 'http://0.0.0.0:80',
     swiftUsername: 'root',
     swiftPassword: 'root'
